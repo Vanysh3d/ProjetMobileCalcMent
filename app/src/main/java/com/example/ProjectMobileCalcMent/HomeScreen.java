674074caf575fcc98da196a957543cb5d9188128
Vanysh3d <louis.plihon@etud.u-picardie.fr>
2024-06-11 10:44:13 +0200

    ajout des bouton et du menu pour le homescreen

--- app/src/main/java/com/example/ProjectMobileCalcMent/HomeScreen.java
+++ app/src/main/java/com/example/ProjectMobileCalcMent/HomeScreen.java
@@ -1,6 +1,8 @@
 package com.example.ProjectMobileCalcMent;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,5 +24,31 @@ public class HomeScreen extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.menu_about) {
+            // Handle the about action
+            return true;
+        } else if (id == R.id.menu_settings) {
+            // Handle the settings action
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
