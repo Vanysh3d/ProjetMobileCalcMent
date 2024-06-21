2884eb14072077ed5711755d5e3c40c91eb709e7
Vanysh3d <louis.plihon@etud.u-picardie.fr>
2024-06-21 13:05:01 +0200

    modification de l'affichage des settings + ajout de fonctionnalités secondaires + début de traduction

--- app/src/main/java/com/example/ProjectMobileCalcMent/HomeScreen.java
+++ app/src/main/java/com/example/ProjectMobileCalcMent/HomeScreen.java
@@ -1,73 +1,191 @@
 package com.example.ProjectMobileCalcMent;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import com.example.projetmobilecalcment.R;
 
+import java.util.Locale;
+
 public class HomeScreen extends AppCompatActivity {
+        MediaPlayer mediaPlayer;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_home_screen);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        private AppBarConfiguration appBarConfiguration;
 
-        Button playButton = findViewById(R.id.button_play);
-        Button scoreButton = findViewById(R.id.button_score);
-        Button quitButton = findViewById(R.id.button_quit);
+        private boolean isLanguageChanged = false;
 
-        playButton.setOnClickListener(v -> {
-            Intent intent = new Intent(this, GameActivity.class);
-            startActivity(intent);
-        });
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            loadLocale();
+            setContentView(R.layout.activity_home_screen);
 
-        scoreButton.setOnClickListener(v -> {
-            Intent intent = new Intent(this, ScoreActivity.class);
-            startActivity(intent);
-        });
+            mediaPlayer = MediaPlayer.create(this, R.raw.Puzzles);
+            mediaPlayer.setLooping(true);
 
-        quitButton.setOnClickListener(v -> {
-            finishAffinity();
-        });
-    }
+            SharedPreferences sharedPreferences = getSharedPreferences("Settings", MODE_PRIVATE);
+            boolean isMusicEnabled = sharedPreferences.getBoolean("MusicEnabled", true);
+            if (isMusicEnabled) {
+                mediaPlayer.start();
+            }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return true;
-    }
+            Button buttonPlay = findViewById(R.id.button_play);
+            buttonPlay.setOnClickListener(v -> {
+                Intent intent = new Intent(HomeScreen.this, GameActivity.class);
+                startActivity(intent);
+            });
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
+            Button buttonScore = findViewById(R.id.button_score);
+            buttonScore.setOnClickListener(v -> {
+                Intent intent = new Intent(HomeScreen.this, ScoreActivity.class);
+                startActivity(intent);
+            });
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.menu_about) {
-            Intent intent = new Intent(this, AboutActivity.class);
-            startActivity(intent);
-            return true;
-        } else if (id == R.id.menu_settings) {
-            Intent intent = new Intent(this, SettingsActivity.class);
-            startActivity(intent);
+            Button buttonQuit = findViewById(R.id.button_quit);
+            buttonQuit.setOnClickListener(v -> {
+                finish();
+                System.exit(0);
+            });
+
+        }
+
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            getMenuInflater().inflate(R.menu.main_menu, menu);
+            TypedValue typedValue = new TypedValue();
+            getTheme().resolveAttribute(com.google.android.material.R.attr.colorOnSecondary, typedValue, true);
+            int color = ContextCompat.getColor(this, typedValue.resourceId);
+
+            for (int i = 0; i < menu.size(); i++) {
+                MenuItem menuItem = menu.getItem(i);
+                SpannableString s = new SpannableString(menuItem.getTitle());
+                s.setSpan(new ForegroundColorSpan(color), 0, s.length(), 0);
+                menuItem.setTitle(s);
+            }
             return true;
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            int id = item.getItemId();
+
+            if (id == R.id.menu_settings) {
+                showSettingsPopup();
+                return true;
+            }
+
+            if (id == R.id.menu_about) {
+                Intent intent = new Intent(this, AboutActivity.class);
+                startActivity(intent);
+                return true;
+            }
+
+            return super.onOptionsItemSelected(item);
+        }
+
+        private void showSettingsPopup() {
+            LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+            View popupView = inflater.inflate(R.layout.popup_settings, null);
+
+            int width = LinearLayout.LayoutParams.MATCH_PARENT;
+            int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+            boolean focusable = true;
+            PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+            // MUSIC
+            SharedPreferences sharedPreferences = getSharedPreferences("Settings", MODE_PRIVATE);
+            boolean isMusicEnabled = sharedPreferences.getBoolean("MusicEnabled", true);
+            SwitchCompat switchMusic = popupView.findViewById(R.id.switch_music);
+            switchMusic.setChecked(isMusicEnabled);
+            switchMusic.setOnCheckedChangeListener((buttonView, isChecked) -> {
+                SharedPreferences.Editor editor = getSharedPreferences("Settings", MODE_PRIVATE).edit();
+                editor.putBoolean("MusicEnabled", isChecked);
+                editor.apply();
+                if (isChecked) {
+                    mediaPlayer.start();
+                } else {
+                    mediaPlayer.pause();
+                }
+            });
+
+            // Afficher le PopupWindow sous la barre d'outils
+            popupWindow.showAtLocation(findViewById(R.id.toolbar), Gravity.TOP | Gravity.START, 0, (getSupportActionBar().getHeight()) + 100);
+            popupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
+                @Override
+                public void onDismiss() {
+                    if (isLanguageChanged) {
+                        recreate();
+                        isLanguageChanged = false;
+                    }
+                    SharedPreferences sharedPreferences = getSharedPreferences("Settings", MODE_PRIVATE);
+                    boolean isMusicEnabled = sharedPreferences.getBoolean("MusicEnabled", true);
+                    if (isMusicEnabled) {
+                        mediaPlayer.start();
+                    } else {
+                        mediaPlayer.pause();
+                    }
+                }
+            });
+
+        }
+
+        public void setLocale(String lang) {
+            String currentLang = getSharedPreferences("Settings", MODE_PRIVATE).getString("My_Lang", "");
+            if (!currentLang.equals(lang)) {
+                isLanguageChanged = true;
+                Locale locale = new Locale(lang);
+                Locale.setDefault(locale);
+                Configuration config = new Configuration();
+                config.locale = locale;
+                getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+                // Save data to shared preferences
+                SharedPreferences.Editor editor = getSharedPreferences("Settings", MODE_PRIVATE).edit();
+                editor.putString("My_Lang", lang);
+                editor.apply();
+            }
+        }
+
+        public void loadLocale() {
+            SharedPreferences prefs = getSharedPreferences("Settings", Activity.MODE_PRIVATE);
+            String language = prefs.getString("My_Lang", "");
+            setLocale(language);
+        }
+
+        @Override
+        protected void onDestroy() {
+            super.onDestroy();
+            if(mediaPlayer != null){
+                mediaPlayer.release();
+                mediaPlayer = null;
+            }
+        }
     }
-}
